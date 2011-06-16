e2a8b7e0177dedf08884e727e3f69c122ef38014
Martin Hribar <hribar.martin@gmail.com>
2011-06-16 16:53:25 +0200

    Clean up, and i test it with 1.6.6 it should work

--- src/org/martin/bukkit/npclib/NPCPath.java
+++ src/org/martin/bukkit/npclib/NPCPath.java
@@ -12,19 +12,19 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 
 /**
-*
-* @author Top_Cat
-*/
+ *
+ * @author Top_Cat
+ */
 public class NPCPath {
-    
+
     public class Node { // Holds data about each block we check
-        
-        int f,g = 0,h;
+
+        int f, g = 0, h;
         int xPos, yPos, zPos;
         Node parent;
         Block b;
         boolean notsolid, liquid;
-        
+
         public Node(Block b) {
             this.b = b;
             xPos = b.getX();
@@ -32,31 +32,26 @@ public class NPCPath {
             zPos = b.getZ();
             update();
         }
-        
+
         public void update() {
             notsolid = standon.contains(b.getType());
             liquid = liquids.contains(b.getType());
         }
-        
     }
-    
     HashMap<Block, Node> nodes = new HashMap<Block, Node>();
-    
     ArrayList<Node> path = new ArrayList<Node>();
     ArrayList<Node> open = new ArrayList<Node>();
     ArrayList<Node> closed = new ArrayList<Node>();
-    
     Comparator<Node> nodeComp = new NodeComparator();
-    
     Node startNode, endNode;
     Material[] nonSolid = {Material.AIR, Material.SAPLING, Material.POWERED_RAIL, Material.DETECTOR_RAIL, Material.RAILS, Material.YELLOW_FLOWER, Material.RED_ROSE, Material.RED_MUSHROOM, Material.BROWN_MUSHROOM, Material.TORCH, Material.FIRE, Material.REDSTONE_WIRE, Material.CROPS, Material.SIGN_POST, Material.WALL_SIGN, Material.LEVER, Material.STONE_BUTTON, Material.STONE_PLATE, Material.WOOD_PLATE, Material.REDSTONE_TORCH_OFF, Material.REDSTONE_TORCH_ON, Material.SUGAR_CANE_BLOCK, Material.DIODE_BLOCK_OFF, Material.DIODE_BLOCK_ON};
     List<Material> standon = new ArrayList<Material>();
     List<Material> liquids = new ArrayList<Material>();
-    
+
     public NPCPath(Location start, Location end, int maxIteractions) {
         init(start, end, maxIteractions);
     }
-        
+
     private void init(Location s, Location e, int max) {
         liquids.add(Material.WATER);
         liquids.add(Material.STATIONARY_WATER);
@@ -65,12 +60,12 @@ public class NPCPath {
         liquids.add(Material.LADDER); // Trust me it makes sense
         standon.addAll(Arrays.asList(nonSolid));
         standon.addAll(liquids);
-        
+
         startNode = getNode(s.getBlock());
         endNode = getNode(e.getBlock());
         look(startNode, max);
     }
-    
+
     public Node getNextNode() {
         if (path.size() > 0) {
             Node r = path.get(0);
@@ -79,14 +74,14 @@ public class NPCPath {
         }
         return null;
     }
-    
+
     private Node getNode(Block b) {
         if (!nodes.containsKey(b)) {
             nodes.put(b, new Node(b));
         }
         return nodes.get(b);
     }
-    
+
     private void look(Node c, int max) {
         Node adjacentBlock;
         int rep = 0;
@@ -94,7 +89,7 @@ public class NPCPath {
             rep++;
             closed.add(c);
             open.remove(c);
-            
+
             for (int i = -1; i <= 1; i++) {
                 for (int j = -1; j <= 1; j++) {
                     for (int k = -1; k <= 1; k++) {
@@ -124,7 +119,7 @@ public class NPCPath {
             path.add(endNode);
         }
     }
-    
+
     public class NodeComparator implements Comparator<Node> {
 
         @Override
@@ -136,57 +131,59 @@ public class NPCPath {
             }
             return 0;
         }
-        
     }
-    
+
     public boolean checkPath(Node node, Node parent) {
         return checkPath(node, parent, false);
     }
-    
+
     public boolean checkPath(Node node, Node parent, boolean update) {
         boolean corner = false;
         if ((node.xPos != parent.xPos && node.zPos != parent.zPos) || (node.xPos != parent.xPos && node.yPos != parent.yPos) || (node.yPos != parent.yPos && node.zPos != parent.zPos)) {
             int xDir = (node.xPos - parent.xPos);
             int zDir = (node.zPos - parent.zPos);
-            
+
             boolean xZCor1 = !getNode(parent.b.getRelative(0, 0, zDir)).notsolid;
             boolean xZCor2 = !getNode(parent.b.getRelative(xDir, 0, 0)).notsolid;
-            
+
             corner = (xZCor1 || xZCor2);
         }
-        
+
         Node nodeBelow = getNode(node.b.getRelative(0, -1, 0));
         Node nodeAbove = getNode(node.b.getRelative(0, 1, 0));
-        
-        if (update) { nodeBelow.update(); nodeAbove.update(); node.update(); }
-        
+
+        if (update) {
+            nodeBelow.update();
+            nodeAbove.update();
+            node.update();
+        }
+
         return !corner && ((node.notsolid && (!nodeBelow.notsolid || (nodeBelow.liquid && node.liquid)) && nodeAbove.notsolid) || node == endNode);
     }
-    
+
     private void scoreBlock(Node node, Node parent) {
-        int diagonal = ((node.xPos != parent.xPos && node.zPos != parent.zPos) || (node.xPos != parent.xPos && node.yPos != parent.yPos) || (node.yPos != parent.yPos && node.zPos != parent.zPos))  ? 14 : 10;
-        
+        int diagonal = ((node.xPos != parent.xPos && node.zPos != parent.zPos) || (node.xPos != parent.xPos && node.yPos != parent.yPos) || (node.yPos != parent.yPos && node.zPos != parent.zPos)) ? 14 : 10;
+
         if (checkPath(node, parent)) {
             if (!open.contains(node) && !closed.contains(node)) {
                 node.parent = parent;
                 node.g = parent.g + diagonal;
-                
+
                 int difX = Math.abs(endNode.xPos - node.xPos);
                 int difY = Math.abs(endNode.yPos - node.yPos);
                 int difZ = Math.abs(endNode.zPos - node.zPos);
-                
+
                 node.h = (difX + difY + difZ) * 10;
                 node.f = node.g + node.h;
-                
+
                 open.add(node);
             } else if (!closed.contains(node)) {
-                 int g = parent.g + diagonal;
-                 if (g < node.g) {
-                     node.g = g;
-                     node.parent = parent;
-                 }
+                int g = parent.g + diagonal;
+                if (g < node.g) {
+                    node.g = g;
+                    node.parent = parent;
+                }
             }
         }
     }
-    
 }
