81425f7af2d085cc5ac905f98d6812f5cea028ca
Thomas Cheyney <webmaster@thomasc.co.uk>
2011-06-20 06:40:33 -0700

    j became l in 1.6 update :)

--- src/org/martin/bukkit/npclib/NPCNetworkManager.java
+++ src/org/martin/bukkit/npclib/NPCNetworkManager.java
@@ -15,11 +15,11 @@ public class NPCNetworkManager extends NetworkManager {
     public NPCNetworkManager(Socket socket, String s, NetHandler nethandler) {
         super(socket, s, nethandler);
         try {
-            Field f = NetworkManager.class.getDeclaredField("j");
+            Field f = NetworkManager.class.getDeclaredField("l");
             f.setAccessible(true);
             f.set(this, false);
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 
