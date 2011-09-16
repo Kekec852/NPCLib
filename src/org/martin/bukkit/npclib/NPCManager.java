339ab50549b758d84796c78033bd6e330d782787
Martin Hribar <hribar.marti@gmail.com>
2011-09-16 19:32:33 +0800

    Update for 1.8; need testing

--- src/org/martin/bukkit/npclib/NPCManager.java
+++ src/org/martin/bukkit/npclib/NPCManager.java
@@ -41,7 +41,7 @@ public class NPCManager {
                 HashSet<String> toRemove = new HashSet<String>();
                 for (String i : npcs.keySet()) {
                     Entity j = npcs.get(i);
-                    j.R();
+                    j.aa();//j.R(); was renamed
                     if (j.dead) {
                         toRemove.add(i);
                     }
