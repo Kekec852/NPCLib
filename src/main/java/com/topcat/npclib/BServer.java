b93c7327991d105b3a7b191c3cacb8af15f43cdb
Ian M <ianmacalinao@gmail.com>
2011-12-13 13:21:40 -0600

    Maven

--- src/main/java/com/topcat/npclib/BServer.java
+++ src/main/java/com/topcat/npclib/BServer.java
@@ -1,4 +1,4 @@
-package org.martin.bukkit.npclib;
+package com.topcat.npclib;
 
 import java.util.HashMap;
 import java.util.List;
--- src/main/java/com/topcat/npclib/NpcEntityTargetEvent.java
+++ src/main/java/com/topcat/npclib/NpcEntityTargetEvent.java
@@ -1,4 +1,4 @@
-package org.martin.bukkit.npclib;
+package com.topcat.npclib;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.event.entity.EntityTargetEvent;
